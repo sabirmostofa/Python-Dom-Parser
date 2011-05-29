41a3adb18e6608f5a1631fb73c3a4bae24e6f76d
sabbir <s@gypsy.(none)>
2011-05-29 18:44:58 +0600

    first

---
+++ python-parser.py
@@ -0,0 +1,24 @@
+import re
+
+def getAttr(text,tag,attr):
+	reString = '<{0}[^>]*>'.format(tag)
+	tag_re = re.compile(reString,re.S)
+	node = tag_re.search(text).group()
+		
+	attrString='(?<={0}=").*?(?=")'.format(attr)
+	attr_re =  re.compile(attrString,re.S)
+	return attr_re.search(node).group()
+	
+	
+def getTag(tag,text):	
+	#only supports <id></id> style tags returns the innerText
+	reString = '(?<=<{0}>).*?(?=</{0}>|/>)'.format(tag)	
+	tag_re = re.compile(reString,re.S) 
+	return tag_re.search(text).group()
+
+def innerText(tag,doc):
+	#supports all kinds of tags like <id style=''>hello</id>
+	reString = '(?<=<{0})[^>]*>(.*?)(?=</{0}>)'.format(tag)	
+	tag_re = re.compile(reString,re.S)
+	return  tag_re.search(doc).group(1) 	
+	
